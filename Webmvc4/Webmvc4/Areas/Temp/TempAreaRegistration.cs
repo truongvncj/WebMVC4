77e287fdcd0f35a9ec645fe2a942d899f3491d33
truongvncj <truongvncj@gmail.com>
2018-11-20 09:52:30 +0700

    cm
    cm

---
+++ Webmvc4/Webmvc4/Areas/Temp/TempAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Webmvc4.Areas.Temp
+{
+    public class TempAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Temp";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Temp_default",
+                "Temp/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
